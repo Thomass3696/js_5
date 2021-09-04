1c7c105c57620e1c07c6c7cf90ae83cd64f8bb09
Thomass3696 <tomas3696@gmail.com>
2021-09-04 18:56:14 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+$('#b_1').click(() => {
     console.log('Yeah, you clicked me.')
+})
+
+$('#b_2').click(() => {
+    $('#b_1').text('Shoot')
+})
+
+$('#b_3').click(() => {
+    $('#b_1').css('background', 'lightpink')
+    $('#b_2').css('background', 'lightgreen')
+    $('#b_3').css('background', 'lightblue')
+})
